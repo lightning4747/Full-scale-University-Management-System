3b5313ee0785d34d5b545a15d5bfb3aaad62c846
lightning4747 <vignesh112847@gmail.com>
2026-01-17 16:54:50 +0530

    Add searchbar

--- classroom-frontend/src/pages/Subjects/list.tsx
+++ classroom-frontend/src/pages/Subjects/list.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import { Breadcrumb } from '@/components/refine-ui/layout/breadcrumb'
+import { ListView } from '@/components/refine-ui/views/list-view'
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
+import React, { useState } from 'react'
 
 const SubjectsList = () => {
+  const [searchQuery, setSearchQuery] = useState('');
   return (
-    <div>
-     Subjects 
-    </div>
+    <ListView>
+      <Breadcrumb />
+
+      <h1 className='page-title'>Subjects</h1>
+
+      <div className='intro-row'>
+        <p>Quick acces to essential metrics and management tools.</p>
+
+        <div className='action-row'>
+          <div className='search-field'>
+            <Search className='search-icon'/>
+
+            <Input type="text" 
+                placeholder='Search by name...' 
+                className='pl-10 w-full'
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+    </ListView>
   )
 }
 
