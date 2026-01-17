beed16fe5795fa247d04dd2a289bc03468e52fcb
lightning4747 <vignesh112847@gmail.com>
2026-01-17 18:24:39 +0530

    shows mock data on Subject page

--- classroom-frontend/src/providers/data.ts
+++ classroom-frontend/src/providers/data.ts
@@ -8,8 +8,8 @@ export const dataProvider: DataProvider = {
         return {data: [] as TData[], total: 0};
       }
       return {
-        data:[],
-        total:0,
+        data:MOCK_SUBJECTS as unknown as TData[],
+        total:MOCK_SUBJECTS.length,
       }
   },
   getOne:async () => {throw new Error ('This function is not present in mock')},
