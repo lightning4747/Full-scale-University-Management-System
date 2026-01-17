864d4f523acb826da0501f5f72fc5281fb4d9615
lightning4747 <vignesh112847@gmail.com>
2026-01-17 17:45:15 +0530

    Add DataTable

--- classroom-frontend/src/pages/Subjects/list.tsx
+++ classroom-frontend/src/pages/Subjects/list.tsx
@@ -1,14 +1,49 @@
-import { Breadcrumb } from '@/components/refine-ui/layout/breadcrumb'
-import { ListView } from '@/components/refine-ui/views/list-view'
-import { Input } from '@/components/ui/input'
-import { Select, SelectItem, SelectContent, SelectValue, SelectTrigger } from '@/components/ui/select'
-import { Search } from 'lucide-react'
-import React, { useState } from 'react'
-import { DEPARTMENT_OPTIONS } from '@/constants'
+import React, { useMemo, useState } from "react";
 
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
+import { DataTable } from "@/components/refine-ui/data-table/data-table";
+import { CreateButton } from "@/components/refine-ui/buttons/create";
+
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectItem,
+  SelectContent,
+  SelectValue,
+  SelectTrigger,
+} from "@/components/ui/select";
+import { Badge } from "@/components/ui/badge";
+
+import { useTable } from "@refinedev/react-table";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Search } from "lucide-react";
+import { DEPARTMENT_OPTIONS } from "@/constants";
+import { Subject } from "@/types";
 const SubjectsList = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [selectedDepartment, setSelectedDepartment] = useState('');
+  const [selectedDepartment, setSelectedDepartment] = useState('all');
+  const subjectTable = useTable<Subject>({
+    columns: useMemo<ColumnDef<Subject>[]>(()=> [
+      {id:'code',
+       accessorKey: 'code', 
+       size:100,
+       header:() => <p className='column-title ml-2'>Code</p>,
+       cell: ({ getValue }) => (
+        <Badge>{getValue<string>()}</Badge>
+      ),
+      }
+    ],[]),
+    refineCoreProps: {
+      resource: 'subjects',
+      pagination: {
+        pageSize: 10,mode: 'server',
+      },
+      filters: {},
+      sorters: {},
+    } 
+  });
   return (
     <ListView>
       <Breadcrumb />
@@ -50,9 +85,12 @@ const SubjectsList = () => {
                 }
               </SelectContent>
             </Select>
+            <CreateButton />
           </div>
         </div>
       </div>
+
+      <DataTable table={subjectTable} />
     </ListView>
   )
 }
