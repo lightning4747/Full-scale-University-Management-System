cb2d385cbcc40eb696dd83b0db66596c81d1a682
lightning4747 <vignesh112847@gmail.com>
2026-01-20 22:36:51 +0530

    implement search function

--- classroom-frontend/src/providers/data.ts
+++ classroom-frontend/src/providers/data.ts
@@ -1,3 +1,4 @@
+import { Pagination } from '@/components/ui/pagination';
 import { BACKEND_BASE_URL } from '@/constants';
 import { ListResponse } from '@/types';
 import {createDataProvider, CreateDataProviderOptions} from '@refinedev/rest';
@@ -6,7 +7,23 @@ const options: CreateDataProviderOptions = {
     getList: {
       getEndpoint: ({ resource }) => resource,
 
-      
+      buildQueryParams: async ({ resource, pagination, filters}) => {
+        const page = pagination?.currentPage ?? 1;
+        const pageSize = pagination?.pageSize ?? 10;
+
+        const params: Record<string, string | number> = { page, limit: pageSize};
+
+        filters?.forEach((filter) => {
+          const field = 'field' in filter ? filter.field: '';
+          const value = String(filter.value);
+
+          if(resource === 'subjects') {
+            if(field === 'department') params.department = value;
+            if(field === 'name' || field === 'code') params.search = value;
+          }
+        })
+        return params;
+      },
 
       mapResponse: async (response) => {
         const payload: ListResponse = await response.json();
