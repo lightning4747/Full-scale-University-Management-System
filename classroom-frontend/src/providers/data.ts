6c2ee68f5374becb3483ba131454e2d154f88a24
lightning4747 <vignesh112847@gmail.com>
2026-01-20 22:26:33 +0530

    update the data.ts to show the data from the db instead of mock data

--- classroom-frontend/src/providers/data.ts
+++ classroom-frontend/src/providers/data.ts
@@ -1,21 +1,25 @@
-import { DataProvider, GetListParams, GetListResponse, BaseRecord } from "@refinedev/core";
-import { MOCK_SUBJECTS } from "@/constants/mock-data";
+import { BACKEND_BASE_URL } from '@/constants';
+import { ListResponse } from '@/types';
+import {createDataProvider, CreateDataProviderOptions} from '@refinedev/rest';
 
-export const dataProvider: DataProvider = {
-  getList: async <TData extends BaseRecord = BaseRecord>({ resource }:
-  GetListParams): Promise<GetListResponse<TData>> => {
-      if(resource !== 'subjects') {
-        return {data: [] as TData[], total: 0};
+const options: CreateDataProviderOptions = {
+    getList: {
+      getEndpoint: ({ resource }) => resource,
+
+      
+
+      mapResponse: async (response) => {
+        const payload: ListResponse = await response.json();
+        return payload.data ?? [];
+      },
+
+      getTotalCount: async (response) => {
+        const payload: ListResponse = await response.json();
+        return payload.pagination?.total ?? payload.data?.length ?? 0;
       }
-      return {
-        data:MOCK_SUBJECTS as unknown as TData[],
-        total:MOCK_SUBJECTS.length,
-      }
-  },
-  getOne:async () => {throw new Error ('This function is not present in mock')},
-  create:async () => {throw new Error ('This function is not present in mock')},
-  update:async () => {throw new Error ('This function is not present in mock')},
-  deleteOne:async () => {throw new Error ('This function is not present in mock')},
-  
-  getApiUrl: () => '',
+    }
 }
+
+const { dataProvider } = createDataProvider(BACKEND_BASE_URL, options);
+
+export {dataProvider};
