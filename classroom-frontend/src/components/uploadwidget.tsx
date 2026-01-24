8f126f93a76eecfeb6dc9ddaa0d652e53d9ec0f2
lightning4747 <vignesh112847@gmail.com>
2026-01-24 23:45:39 +0530

    defined the basic structure of the widget

--- classroom-frontend/src/components/uploadwidget.tsx
+++ classroom-frontend/src/components/uploadwidget.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-const UploadWidget = () => {
+const UploadWidget = ({ value = null, onChange, disabled = false}) => {
+  const widgetRef = useRef<CloudinaryWidget | null>(null);
+  const onChageRef = useRef(onChange);
+  //it either shows the preview of the already uploaded image or show a upload image image , so it needs a state to manage it
+  const [preview, setPreview] = useState(value);
+  //to let the user delete the uploaded file
+  const [deleteToken, setDeleteToken] = useState<string | null>(null);
+  const [isRemoving, setIsRemoving] = useState(false);
+
+  const openWidget = () => {
+    if(!disabled) {
+      widgetRef.current?.open();
+    }
+  }
+
+  const removeFromCloudinary = async () => {
+    
+  }
   return (
-    <div>upload-widget</div>
+    <div className='space-y-2'></div>
   )
 }
 
