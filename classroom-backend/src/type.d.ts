3c4f13075aa7560898003f71fa00fae5a2d51ae6
lightning4747 <vignesh112847@gmail.com>
2026-02-04 20:58:18 +0530

    add type for Schedule

--- classroom-backend/src/type.d.ts
+++ classroom-backend/src/type.d.ts
@@ -1,2 +1,9 @@
-type UserRoles = "admin" | "teacher" | "student";
-type RateLimitRole = UserRoles | "guest";
+type Schedule = {
+    day: string;
+    startTime: string;
+    endTime: string;
+  };
+  
+  type UserRoles = "admin" | "teacher" | "student";
+  
+  type RateLimitRole = UserRoles | "guest";
