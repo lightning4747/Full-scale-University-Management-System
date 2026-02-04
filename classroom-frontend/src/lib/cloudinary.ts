51ade16406fb0d6ef55536ae0fbb607a94a96adc
lightning4747 <vignesh112847@gmail.com>
2026-02-04 17:47:58 +0530

    add the feature to show banner image of the class using cloudinary

---
+++ classroom-frontend/src/lib/cloudinary.ts
@@ -0,0 +1,45 @@
+import { Cloudinary } from "@cloudinary/url-gen";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+import { compass } from "@cloudinary/url-gen/qualifiers/gravity";
+
+import { TextStyle } from "@cloudinary/url-gen/qualifiers/textStyle";
+import { format, quality, dpr } from "@cloudinary/url-gen/actions/delivery";
+import { source } from "@cloudinary/url-gen/actions/overlay";
+import { text } from "@cloudinary/url-gen/qualifiers/source";
+import { Position } from "@cloudinary/url-gen/qualifiers/position";
+
+import { CLOUDINARY_CLOUD_NAME } from "@/constants";
+
+// Cloudinary instance.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: CLOUDINARY_CLOUD_NAME,
+  },
+});
+
+export const bannerPhoto = (imageCldPubId: string, name: string) => {
+  return (
+    cld
+      .image(imageCldPubId)
+
+      .resize(
+        fill()
+      )
+      // Optimize for web
+      .delivery(format("auto"))
+      .delivery(quality("auto"))
+      .delivery(dpr("auto"))
+      // Text overlay with name
+      .overlay(
+        source(
+          text(name, new TextStyle("roboto", 42).fontWeight("bold")).textColor(
+            "white"
+          )
+        ).position(
+          new Position()
+            .gravity(compass("west"))
+            .offsetX(0.02)
+        )
+      )
+  );
+};
